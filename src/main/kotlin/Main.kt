c4b2af297c6c6b99e470fbf280c17afe7d6c57f5
vkehfdl1 <vkehfdl1@gmail.com>
2023-06-02 23:08:00 +0900

    javalin server convert file to txt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,4 @@
+import io.javalin.Javalin
 import kr.dogfoot.hwplib.`object`.HWPFile
 import kr.dogfoot.hwplib.reader.HWPReader
 import kr.dogfoot.hwplib.tool.textextractor.TextExtractMethod
@@ -5,14 +6,31 @@ import kr.dogfoot.hwplib.tool.textextractor.TextExtractor
 import java.io.File
 
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val app = Javalin.create().start(7000)
 
-    val sampleFilepath = "sample_hwp" + File.separator + "chchangpae.hwp"
-    val hwpFile = readHwp(sampleFilepath)
-    println(hwpFile?.let { extractTextHwp(it) })
+    app.post("/upload") { ctx ->
+        val file = ctx.uploadedFile("file") ?: throw IllegalArgumentException("No file uploaded")
+        val savedFilepath = "sample_hwp/${file.filename()}"
+        val savedFile = File(savedFilepath)
+        file.content().copyTo(savedFile.outputStream())
+
+        val hwpFile = readHwp(savedFile.path)
+        val hwpText = hwpFile?.let {
+            extractTextHwp(it)
+        }
+
+        hwpText?.let {
+            removeFile(savedFilepath)
+            ctx.contentType("text/plain")
+            ctx.result(it)
+        } ?: {
+            ctx.status(400)
+            ctx.result("extract file failed")
+        }
+    }
 }
 
-private fun readHwp(filepath: String) : HWPFile? {
+private fun readHwp(filepath: String): HWPFile? {
     val hwpFile: HWPFile = HWPReader.fromFile(filepath)
     return if (hwpFile.bodyText.sectionList.size > 0) {
         println("$filepath read success!")
@@ -23,6 +41,13 @@ private fun readHwp(filepath: String) : HWPFile? {
     }
 }
 
-private fun extractTextHwp(hwpFile: HWPFile) : String {
+private fun extractTextHwp(hwpFile: HWPFile): String {
     return TextExtractor.extract(hwpFile, TextExtractMethod.InsertControlTextBetweenParagraphText)
+}
+
+private fun removeFile(filepath: String) {
+    val file = File(filepath)
+    if (file.exists()) {
+        file.delete()
+    }
 }
